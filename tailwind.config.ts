c0098db33b8690d5e883b46bebd019cbf0deab46
kypro34 <sonbappe89@gmail.com>
2025-01-10 11:27:31 +0700

    jj

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,23 +8,23 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	container: {
-  		center: 'true',
-  		padding: {
-  			DEFAULT: '20px',
-  			lg: '80px'
-  		},
-  		screens: {
-  			sm: '375px',
-  			md: '768px',
-  			lg: '1200px'
-  		}
-  	},
-  	screens: {
-  		sm: '375px',
-  		md: '768px',
-  		lg: '1200px'
-  	},
+    container: {
+      center: true, 
+      padding: {
+        DEFAULT: '20px',
+        lg: '80px'
+      },
+      screens: {
+        sm: '375px',
+        md: '768px',
+        lg: '1200px'
+      }
+    },
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1200px'
+    },
   	extend: {
   		borderRadius: {
   			lg: 'var(--radius)',
